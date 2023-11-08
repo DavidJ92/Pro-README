2d0012ff038284bd2d26a8fabe903b27b4498813
DavidJ92 <141689412+DavidJ92@users.noreply.github.com>
2023-11-07 18:59:57 -0600

    update index.js/generatemarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,85 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to return a license badge based on the license type
+function renderLicenseBadge(license) {
+  if (license) {
+    return `![License](https://img.shields.io/badge/license-${license}-brightgreen)`;
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function to return the license link
+function renderLicenseLink(license) {
+  if (license) {
+    return `[${license} License](https://opensource.org/licenses/${license})`;
+  }
+  return "";
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function to return the license section of README
+function renderLicenseSection(license) {
+  if (license) {
+    return `This project is licensed under the ${renderLicenseLink(license)} license.`;
+  }
+  return "";
+}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+${data.credits}
+
+## License
+
+${renderLicenseBadge(data.license)}
+
+${renderLicenseSection(data.license)}
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+${data.badges}
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+${data.features}
+
+## How to Contribute
+
+${data.contribution}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For additional questions, please contact [GitHub: ${data.github}](https://github.com/${data.github}) or email at ${data.email}.
 
 `;
 }
--- Develop/utils/index.js
+++ Develop/utils/index.js
@@ -1,13 +1,85 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./generateMarkdown");
+const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Provide a description of your project:",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Provide installation instructions:",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Provide usage information:",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Provide credits (if any):",
+    name: "credits",
+  },
+  {
+    type: "list",
+    message: "Choose a license for your application:",
+    choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "None"],
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "Provide badges (if any):",
+    name: "badges",
+  },
+  {
+    type: "input",
+    message: "List features of your project:",
+    name: "features",
+  },
+  {
+    type: "input",
+    message: "Provide contribution guidelines:",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "Provide test instructions:",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    const markdownContent = generateMarkdown(data);
+    writeToFile("README.md", markdownContent);
+    console.log("README.md successfully generated!");
+  });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
 
-// Function call to initialize app
 init();
